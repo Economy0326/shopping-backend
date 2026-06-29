6a2c81f3fb6d17a12ba4c57972c16d3281ed8a25
Economy0326 <wjdrudwo0575@naver.com>
2026-06-29 19:39:59 +0900

    chore: resolve production audit issues

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -13,13 +13,19 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api/v1');
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/api/v1/health (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api/v1/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect({ ok: true });
   });
 });
